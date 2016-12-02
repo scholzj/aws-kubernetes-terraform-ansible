bc26cc51c93505cf299382cd3cb59e52032e687e
Jakub Scholz <www@scholzj.com>
2016-12-02 14:19:54 +0100

    Move certificate dir into ansible, so that it gets copied with ansible scripts

--- terraform/certificates.tf
+++ terraform/certificates.tf
@@ -22,10 +22,10 @@ resource "null_resource" "certificates" {
   }
 
   provisioner "local-exec" {
-    command = "echo '${ data.template_file.certificates.rendered }' > ../cert/kubernetes-csr.json"
+    command = "echo '${ data.template_file.certificates.rendered }' > ../ansible/cert/kubernetes-csr.json"
   }
 
   provisioner "local-exec" {
-    command = "cd ../cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
+    command = "cd ../ansible/cert; cfssl gencert -initca ca-csr.json | cfssljson -bare ca; cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=kubernetes kubernetes-csr.json | cfssljson -bare kubernetes"
   }
 }
