b33f0046810539d059551e28ff4a61b3f07b7b22
Jakub Scholz <www@scholzj.com>
2016-12-05 22:54:40 +0100

    Fix typo in template names

--- terraform/bootstrap.tf
+++ terraform/bootstrap.tf
@@ -3,7 +3,7 @@
 ####################
 
 data "template_file" "jumphost-bootstrap-script" {
-    template = "${file("${path.module}/template/jumphost-boostrap.sh.tpl")}"
+    template = "${file("${path.module}/template/jumphost-bootstrap.sh.tpl")}"
     vars {
       region = "${var.region}"
       s3_bucket = "${var.vpc_name}-bootstrap"
@@ -12,7 +12,7 @@ data "template_file" "jumphost-bootstrap-script" {
 }
 
 data "template_file" "etcd-bootstrap-script" {
-    template = "${file("${path.module}/template/etcd-boostrap.sh.tpl")}"
+    template = "${file("${path.module}/template/etcd-bootstrap.sh.tpl")}"
     vars {
       region = "${var.region}"
       s3_bucket = "${var.vpc_name}-bootstrap"
@@ -21,7 +21,7 @@ data "template_file" "etcd-bootstrap-script" {
 }
 
 data "template_file" "controller-bootstrap-script" {
-    template = "${file("${path.module}/template/controller-boostrap.sh.tpl")}"
+    template = "${file("${path.module}/template/controller-bootstrap.sh.tpl")}"
     vars {
       region = "${var.region}"
       s3_bucket = "${var.vpc_name}-bootstrap"
@@ -30,7 +30,7 @@ data "template_file" "controller-bootstrap-script" {
 }
 
 data "template_file" "worker-bootstrap-script" {
-    template = "${file("${path.module}/template/worker-boostrap.sh.tpl")}"
+    template = "${file("${path.module}/template/worker-bootstrap.sh.tpl")}"
     vars {
       region = "${var.region}"
       s3_bucket = "${var.vpc_name}-bootstrap"
